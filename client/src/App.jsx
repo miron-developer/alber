fe9191bc7a387e148bd3aff4b75c10f7c9674681
miron-developer <miron.arystan@mail.ru>
2021-09-10 22:55:31 +0600

    feat: start client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     zhibek kz
+    </div>
+  );
+}
+
+export default App;
---
+++ server/db/migrations/0019_test_images.up.sql
@@ -0,0 +1,15 @@
+INSERT INTO Images VALUES (
+    null,
+    '/assets/img/logo192.png', --src
+    'kkjh51.jpg', --name
+    1, --userID
+    1 --parselID
+);
+
+INSERT INTO Images VALUES (
+    null,
+    '/assets/img/logo192.png', --src
+    'Sdf845.jpeg', --name
+    2, --userID
+    2 --parselID
+);
